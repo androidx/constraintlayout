d52d99dcb297ab7d8be823e25ac1bc034692d526
jafu888 <hoford@google.com>
2020-09-25 22:08:46 -0700

    Add pan/zoom/rotate to ImageFilterView/Button

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/utils/widget/ImageFilterButton.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/utils/widget/ImageFilterButton.java
@@ -18,6 +18,7 @@ package androidx.constraintlayout.utils.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Outline;
 import android.graphics.Path;
 import android.graphics.RectF;
@@ -125,11 +126,23 @@ public class ImageFilterButton extends androidx.appcompat.widget.AppCompatImageB
                 } else if (attr == R.styleable.ImageFilterView_contrast) {
                     setContrast(a.getFloat(attr, 0));
                 } else if (attr == R.styleable.ImageFilterView_round) {
-                    setRound(a.getDimension(attr, 0));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        setRound(a.getDimension(attr, 0));
+                    }
                 } else if (attr == R.styleable.ImageFilterView_roundPercent) {
-                    setRoundPercent(a.getFloat(attr, 0));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        setRoundPercent(a.getFloat(attr, 0));
+                    }
                 } else if (attr == R.styleable.ImageFilterView_overlay) {
                     setOverlay(a.getBoolean(attr, mOverlay));
+                } else if (attr == R.styleable.ImageFilterView_imagePanX) {
+                    setImagePanX(a.getFloat(attr, mPanX));
+                } else if (attr == R.styleable.ImageFilterView_imagePanY) {
+                    setImagePanY(a.getFloat(attr, mPanY));
+                } else if (attr == R.styleable.ImageFilterView_imageRotate) {
+                    setImageRotate(a.getFloat(attr, mRotate));
+                } else if (attr == R.styleable.ImageFilterView_imageZoom) {
+                    setImageZoom(a.getFloat(attr, mZoom));
                 }
             }
             a.recycle();
@@ -145,7 +158,104 @@ public class ImageFilterButton extends androidx.appcompat.widget.AppCompatImageB
             }
         }
     }
+    // ======================== support for pan/zoom/rotate =================
+    // defined as 0 = center of screen
+    // if with < scree with,  1 is the right edge lines up with screen
+    // if width > screen width, 1 is thee left edge lines up
+    // -1 works similarly
+    // zoom 1 = the image fits such that the view is filed
 
+    private float mPanX = Float.NaN;
+    private float mPanY = Float.NaN;
+    private float mZoom = Float.NaN;
+    private float mRotate = Float.NaN;
+    /**
+     * sets the pan from the center
+     * pan of 1 the image is "all the way to the right"
+     * if the images width is greater than the screen width, pan = 1 results in the left edge lining up
+     * if the images width is less than the screen width, pan = 1 results in the right edges lining up
+     * if image width == screen width it does nothing
+     * @return the pan in X. Where 0 is centered = Float.NaN if not set
+     */
+    public float getImagePanX( ) {
+        return  mPanX;
+    }
+    public  float getImagePanY( ) {
+        return mPanY;
+    }
+    public  float getImageZoom( ) {
+        return mZoom;
+    }
+    public  float getImageRotate( ) {
+        return mRotate;
+    }
+
+    /**
+     * sets the pan from the center
+     * pan of 1 the image is "all the way to the right"
+     * if the images width is greater than the screen width, pan = 1 results in the left edge lining up
+     * if the images width is less than the screen width, pan = 1 results in the right edges lining up
+     * if image width == screen width it does nothing
+     * @param pan  sets the pan in X. Where 0 is centered
+     */
+    public void setImagePanX(float pan) {
+        mPanX = pan;
+        updateViewMatrix();
+    }
+    /**
+     * sets the pan from the center
+     * pan of 1 the image is "all the way to the right"
+     * if the images width is greater than the screen width, pan = 1 results in the left edge lining up
+     * if the images width is less than the screen width, pan = 1 results in the right edges lining up
+     * if image width == screen width it does nothing
+     * @param pan  sets the pan in X. Where 0 is centered
+     */
+    public  void setImagePanY(float pan) {
+        mPanY = pan;
+        updateViewMatrix();
+    }
+    /**
+     * sets the zoom where 1 scales the image just enough to fill the view
+     * @param zoom  sets the pan in X. Where 0 is centered
+     */
+    public  void setImageZoom(float zoom) {
+        mZoom = zoom;
+        updateViewMatrix();
+    }
+    public  void setImageRotate(float rotation) {
+        mRotate = rotation;
+        updateViewMatrix();
+    }
+
+    private void  updateViewMatrix() {
+        if (Float.isNaN(mPanX) &&
+                Float.isNaN(mPanY) &&
+                Float.isNaN(mZoom) &&
+                Float.isNaN(mRotate)
+        ) {
+            setScaleType(ScaleType.FIT_CENTER);
+            return;
+        }
+        float panX = (Float.isNaN(mPanX)) ? 0 : mPanX;
+        float panY = (Float.isNaN(mPanY)) ? 0 : mPanY;
+        float zoom = (Float.isNaN(mZoom)) ? 0 : mZoom;
+        float rota = (Float.isNaN(mRotate)) ? 0 : mRotate;
+        Matrix imageMatrix = new Matrix();
+        imageMatrix.reset();
+        float iw = getDrawable().getIntrinsicWidth();
+        float ih = getDrawable().getIntrinsicHeight();
+        float sw = getWidth();
+        float sh = getHeight();
+        float scale = zoom * ((iw * sh < ih * sw) ? sw / iw : sh / ih);
+        imageMatrix.postScale(scale, scale);
+        float tx = 0.5f * (panX * (sw-scale*iw) + sw - (scale * iw));
+        float ty = 0.5f * (panY * (sh-scale*ih) + sh - (scale * ih));
+        imageMatrix.postTranslate(tx, ty);
+        imageMatrix.postRotate(rota, sw / 2, sh / 2);
+        setImageMatrix(imageMatrix);
+        setScaleType(ScaleType.MATRIX);
+    }
+    // ================================================================
     /**
      * Defines whether the alt image will be faded in on top of the original image or if it will be
      * crossfaded with it. Default is true.
