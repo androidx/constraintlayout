b0ff3be3d85dca28270d2101dc656ce9fb03c0c9
Jason Pearson <jason.d.pearson@gmail.com>
2020-12-29 21:28:34 -0600

    remove code for old versions and prevent using new api version code that is not completely backwards compatible

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
@@ -966,7 +966,7 @@ public class ConstraintLayout extends ViewGroup {
                             mConstraintLayoutSpec = null;
                         }
                     }
-                } else if (attr == R.styleable.ConstraintLayout_Layout_constraintSet) {
+                } else if (attr == R.styleable.ConstraintLayout_constraintSet) {
                     int id = a.getResourceId(attr, 0);
                     try {
                         mConstraintSet = new ConstraintSet();
@@ -997,9 +997,6 @@ public class ConstraintLayout extends ViewGroup {
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
         super.addView(child, index, params);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            onViewAdded(child);
-        }
     }
 
     /**
@@ -1008,9 +1005,6 @@ public class ConstraintLayout extends ViewGroup {
     @Override
     public void removeView(View view) {
         super.removeView(view);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            onViewRemoved(view);
-        }
     }
 
     /**
@@ -1018,9 +1012,7 @@ public class ConstraintLayout extends ViewGroup {
      */
     @Override
     public void onViewAdded(View view) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            super.onViewAdded(view);
-        }
+        super.onViewAdded(view);
         ConstraintWidget widget = getViewWidget(view);
         if (view instanceof androidx.constraintlayout.widget.Guideline) {
             if (!(widget instanceof Guideline)) {
@@ -1048,9 +1040,7 @@ public class ConstraintLayout extends ViewGroup {
      */
     @Override
     public void onViewRemoved(View view) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            super.onViewRemoved(view);
-        }
+        super.onViewRemoved(view);
         mChildrenByIds.remove(view.getId());
         ConstraintWidget widget = getViewWidget(view);
         mLayoutWidget.remove(widget);
@@ -1638,28 +1628,22 @@ public class ConstraintLayout extends ViewGroup {
         int androidLayoutWidth = measuredWidth + widthPadding;
         int androidLayoutHeight = measuredHeight + heightPadding;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            int resolvedWidthSize = resolveSizeAndState(androidLayoutWidth, widthMeasureSpec, childState);
-            int resolvedHeightSize = resolveSizeAndState(androidLayoutHeight, heightMeasureSpec,
-                    childState << MEASURED_HEIGHT_STATE_SHIFT);
-            resolvedWidthSize &= MEASURED_SIZE_MASK;
-            resolvedHeightSize &= MEASURED_SIZE_MASK;
-            resolvedWidthSize = Math.min(mMaxWidth, resolvedWidthSize);
-            resolvedHeightSize = Math.min(mMaxHeight, resolvedHeightSize);
-            if (isWidthMeasuredTooSmall) {
-                resolvedWidthSize |= MEASURED_STATE_TOO_SMALL;
-            }
-            if (isHeightMeasuredTooSmall) {
-                resolvedHeightSize |= MEASURED_STATE_TOO_SMALL;
-            }
-            setMeasuredDimension(resolvedWidthSize, resolvedHeightSize);
-            mLastMeasureWidth = resolvedWidthSize;
-            mLastMeasureHeight = resolvedHeightSize;
-        } else {
-            setMeasuredDimension(androidLayoutWidth, androidLayoutHeight);
-            mLastMeasureWidth = androidLayoutWidth;
-            mLastMeasureHeight = androidLayoutHeight;
+        int resolvedWidthSize = resolveSizeAndState(androidLayoutWidth, widthMeasureSpec, childState);
+        int resolvedHeightSize = resolveSizeAndState(androidLayoutHeight, heightMeasureSpec,
+                childState << MEASURED_HEIGHT_STATE_SHIFT);
+        resolvedWidthSize &= MEASURED_SIZE_MASK;
+        resolvedHeightSize &= MEASURED_SIZE_MASK;
+        resolvedWidthSize = Math.min(mMaxWidth, resolvedWidthSize);
+        resolvedHeightSize = Math.min(mMaxHeight, resolvedHeightSize);
+        if (isWidthMeasuredTooSmall) {
+            resolvedWidthSize |= MEASURED_STATE_TOO_SMALL;
         }
+        if (isHeightMeasuredTooSmall) {
+            resolvedHeightSize |= MEASURED_STATE_TOO_SMALL;
+        }
+        setMeasuredDimension(resolvedWidthSize, resolvedHeightSize);
+        mLastMeasureWidth = resolvedWidthSize;
+        mLastMeasureHeight = resolvedHeightSize;
     }
 
     /**
@@ -2594,7 +2578,7 @@ public class ConstraintLayout extends ViewGroup {
         float resolvedGuidePercent;
 
         boolean isRtl = false;
-        int layoutDirection = View.LAYOUT_DIRECTION_LTR;
+        int layoutDirection = 0;
 
         ConstraintWidget widget = new ConstraintWidget();
 
