836d9ce9125062943978ef453075ae0dc2cbe225
Jason Pearson <jason.d.pearson@gmail.com>
2021-01-02 17:20:51 -0600

    Refactor unit test package name from android.support to androidx (#67)
    * add mavenCentral so testng can resolve
    
    * upgrade testng to latest stable version
    
    * add jvm arg to allow local xml file loading
    
    * upgraded agp, java compatibility and kotlin version
    
    * latest agp requires useAndroidX flag
    
    * latest agp requires updated gradle wrapper
    
    * move unit tests into androidx package
    
    Co-authored-by: Nicolas Roard <nro@google.com>

---
+++ constraintlayout/core/src/test/java/androidx/constraintlayout/core/AmountTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.constraintlayout.core;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class AmountTest {
+    Amount a1 = new Amount(2, 3);
+    Amount a2 = new Amount(3, 5);
+
+    @BeforeMethod
+    public void setUp() {
+        a1.set(2, 3);
+        a2.set(3, 5);
+    }
+
+    @Test
+    public void testAdd() {
+        a1.add(a2);
+        assertEquals(a1.getNumerator(), 19);
+        assertEquals(a1.getDenominator(), 15);
+    }
+
+    @Test
+    public void testSubstract() {
+        a1.substract(a2);
+        assertEquals(a1.getNumerator(), 1);
+        assertEquals(a1.getDenominator(), 15);
+    }
+
+    @Test
+    public void testMultiply() {
+        a1.multiply(a2);
+        assertEquals(a1.getNumerator(), 2);
+        assertEquals(a1.getDenominator(), 5);
+    }
+
+    @Test
+    public void testDivide() {
+        a1.divide(a2);
+        assertEquals(a1.getNumerator(), 10);
+        assertEquals(a1.getDenominator(), 9);
+    }
+
+    @Test
+    public void testSimplify() {
+        a1.set(20, 30);
+        assertEquals(a1.getNumerator(), 2);
+        assertEquals(a1.getDenominator(), 3);
+        a1.set(77, 88);
+        assertEquals(a1.getNumerator(), 7);
+        assertEquals(a1.getDenominator(), 8);
+    }
+
+    @Test
+    public void testEquality() {
+        a2.set(a1.getNumerator(), a1.getDenominator());
+        assertTrue(a1.equals(a2));
+    }
+}
---
+++ constraintlayout/core/src/test/java/androidx/constraintlayout/core/PriorityTest.java
@@ -0,0 +1,118 @@
+package androidx.constraintlayout.core;
+
+import androidx.constraintlayout.core.widgets.ConstraintAnchor;
+import androidx.constraintlayout.core.widgets.ConstraintWidget;
+import androidx.constraintlayout.core.widgets.ConstraintWidgetContainer;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class PriorityTest {
+
+    @Test
+    public void testPriorityChainHorizontal() {
+        ConstraintWidgetContainer root = new ConstraintWidgetContainer(0, 0, 1000, 600);
+        ConstraintWidget A = new ConstraintWidget(400, 20);
+        ConstraintWidget B = new ConstraintWidget(100, 20);
+        ConstraintWidget C = new ConstraintWidget(100, 20);
+        root.setDebugName("root");
+        A.setDebugName("A");
+        B.setDebugName("B");
+        C.setDebugName("C");
+        root.add(A);
+        root.add(B);
+        root.add(C);
+        A.connect(ConstraintAnchor.Type.LEFT, root, ConstraintAnchor.Type.LEFT);
+        A.connect(ConstraintAnchor.Type.RIGHT, root, ConstraintAnchor.Type.RIGHT);
+
+        B.connect(ConstraintAnchor.Type.LEFT, A, ConstraintAnchor.Type.LEFT);
+        B.connect(ConstraintAnchor.Type.RIGHT, C, ConstraintAnchor.Type.LEFT);
+
+        C.connect(ConstraintAnchor.Type.LEFT, B, ConstraintAnchor.Type.RIGHT);
+        C.connect(ConstraintAnchor.Type.RIGHT, A, ConstraintAnchor.Type.RIGHT);
+
+        B.setHorizontalChainStyle(ConstraintWidget.CHAIN_PACKED);
+        root.layout();
+        System.out.println("a) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getWidth(), 400);
+        assertEquals(B.getWidth(), 100);
+        assertEquals(C.getWidth(), 100);
+        assertEquals(A.getLeft(), 300);
+        assertEquals(B.getLeft(), 400);
+        assertEquals(C.getLeft(), 500);
+
+        B.setHorizontalChainStyle(ConstraintWidget.CHAIN_SPREAD);
+        root.layout();
+        System.out.println("b) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getWidth(), 400);
+        assertEquals(B.getWidth(), 100);
+        assertEquals(C.getWidth(), 100);
+        assertEquals(A.getLeft(), 300);
+        assertEquals(B.getLeft(), 367);
+        assertEquals(C.getLeft(), 533);
+
+        B.setHorizontalChainStyle(ConstraintWidget.CHAIN_SPREAD_INSIDE);
+        root.layout();
+        System.out.println("c) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getWidth(), 400);
+        assertEquals(B.getWidth(), 100);
+        assertEquals(C.getWidth(), 100);
+        assertEquals(A.getLeft(), 300);
+        assertEquals(B.getLeft(), 300);
+        assertEquals(C.getLeft(), 600);
+    }
+
+    @Test
+    public void testPriorityChainVertical() {
+        ConstraintWidgetContainer root = new ConstraintWidgetContainer(0, 0, 1000, 1000);
+        ConstraintWidget A = new ConstraintWidget(400, 400);
+        ConstraintWidget B = new ConstraintWidget(100, 100);
+        ConstraintWidget C = new ConstraintWidget(100, 100);
+        root.setDebugName("root");
+        A.setDebugName("A");
+        B.setDebugName("B");
+        C.setDebugName("C");
+        root.add(A);
+        root.add(B);
+        root.add(C);
+        A.connect(ConstraintAnchor.Type.TOP, root, ConstraintAnchor.Type.TOP);
+        A.connect(ConstraintAnchor.Type.BOTTOM, root, ConstraintAnchor.Type.BOTTOM);
+
+        B.connect(ConstraintAnchor.Type.TOP, A, ConstraintAnchor.Type.TOP);
+        B.connect(ConstraintAnchor.Type.BOTTOM, C, ConstraintAnchor.Type.TOP);
+
+        C.connect(ConstraintAnchor.Type.TOP, B, ConstraintAnchor.Type.BOTTOM);
+        C.connect(ConstraintAnchor.Type.BOTTOM, A, ConstraintAnchor.Type.BOTTOM);
+
+        B.setVerticalChainStyle(ConstraintWidget.CHAIN_PACKED);
+        root.layout();
+        System.out.println("a) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getHeight(), 400);
+        assertEquals(B.getHeight(), 100);
+        assertEquals(C.getHeight(), 100);
+        assertEquals(A.getTop(), 300);
+        assertEquals(B.getTop(), 400);
+        assertEquals(C.getTop(), 500);
+
+        B.setVerticalChainStyle(ConstraintWidget.CHAIN_SPREAD);
+        root.layout();
+        System.out.println("b) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getHeight(), 400);
+        assertEquals(B.getHeight(), 100);
+        assertEquals(C.getHeight(), 100);
+        assertEquals(A.getTop(), 300);
+        assertEquals(B.getTop(), 367);
+        assertEquals(C.getTop(), 533);
+
+        B.setVerticalChainStyle(ConstraintWidget.CHAIN_SPREAD_INSIDE);
+        root.layout();
+        System.out.println("c) root: " + root + " A: " + A + " B: " + B + " C: " + C);
+        assertEquals(A.getHeight(), 400);
+        assertEquals(B.getHeight(), 100);
+        assertEquals(C.getHeight(), 100);
+        assertEquals(A.getTop(), 300);
+        assertEquals(B.getTop(), 300);
+        assertEquals(C.getTop(), 600);
+    }
+}
+
