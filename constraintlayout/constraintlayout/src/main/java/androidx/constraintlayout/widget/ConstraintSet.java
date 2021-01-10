2cdcac7394ae96855035321433021f62d249eee5
Jason Pearson <jason.d.pearson@gmail.com>
2021-01-10 09:54:24 -0600

    deprecate method that is not kotlin friendly and link to new one

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintSet.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintSet.java
@@ -616,7 +616,7 @@ public class ConstraintSet {
                     constraint.layout.mReferenceIds = ((ConstraintHelper) view).getReferencedIds();
                     if (view instanceof Barrier) {
                         Barrier barrier = (Barrier) view;
-                        constraint.layout.mBarrierAllowsGoneWidgets = barrier.allowsGoneWidget();
+                        constraint.layout.mBarrierAllowsGoneWidgets = barrier.getAllowsGoneWidget();
                         constraint.layout.mBarrierDirection = barrier.getType();
                         constraint.layout.mBarrierMargin = barrier.getMargin();
                     }
@@ -2174,7 +2174,7 @@ public class ConstraintSet {
             }
             if (view instanceof Barrier) {
                 Barrier barrier = ((Barrier) view);
-                constraint.layout.mBarrierAllowsGoneWidgets = barrier.allowsGoneWidget();
+                constraint.layout.mBarrierAllowsGoneWidgets = barrier.getAllowsGoneWidget();
                 constraint.layout.mReferenceIds = barrier.getReferencedIds();
                 constraint.layout.mBarrierDirection = barrier.getType();
                 constraint.layout.mBarrierMargin = barrier.getMargin();
