be619132a22a7a5874c74b58db12594345662486
Jason Pearson <jason.d.pearson@gmail.com>
2021-01-17 11:52:01 -0600

    convert desktop projects to use Gradle so they are easier to build

---
+++ desktop/CycleEditor/src/test/java/androidx/motionlayout/cycleeditor/SampleTest.java
@@ -0,0 +1,11 @@
+package androidx.motionlayout.cycleeditor;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testSomething() {
+
+    }
+}
---
+++ desktop/ValidationTool/src/test/java/androidx/constraintlayout/validation/SampleTest.java
@@ -0,0 +1,11 @@
+package androidx.constraintlayout.validation;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testSomething() {
+
+    }
+}
