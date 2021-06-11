6c916671aa051d244d01f4e86de5467d30d8a320
jafu888 <hoford@google.com>
2021-06-10 17:06:59 -0700

    make main ui cycle through composable

--- projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,15 +1,62 @@
 package com.example.constraintlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.constraintlayout.compose.ScreenExample6
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+import androidx.constraintlayout.compose.*
 
 class MainActivity : AppCompatActivity() {
+    private var mFrameLayout: FrameLayout? = null
+    private var composeNum = 6
+    private var MAX = 12
+
+    private fun show(com: ComposeView) {
+        com.setContent() {
+            when (composeNum) {
+                0 -> ScreenExample()
+                1 -> ScreenExample()
+                2 -> ScreenExample2()
+                3 -> ScreenExample3()
+                4 -> ScreenExample4()
+                5 -> ScreenExample5()
+                6 -> ScreenExample6()
+                7 -> ScreenExample7()
+                8 -> ScreenExample8()
+                9 -> ScreenExample9()
+                10 -> ScreenExample10()
+                11 -> ScreenExample11()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ScreenExample6()
+        setContentView(R.layout.activity_main)
+        mFrameLayout = findViewById<FrameLayout>(R.id.frame)
+        setCompose();
+    }
+
+    fun setCompose() {
+        if (mFrameLayout!!.childCount > 0) {
+            mFrameLayout!!.removeAllViews()
         }
+        title = " example " + composeNum;
+        findViewById<TextView>(R.id.layoutName).text = " example " + composeNum;
+        var com = ComposeView(this);
+        mFrameLayout!!.addView(com)
+        show(com)
+    }
+
+    fun prev(view: View) {
+        composeNum = (composeNum + MAX - 1) % MAX
+        setCompose()
+    }
+
+    fun next(view: View) {
+        composeNum = (composeNum + 1) % MAX
+        setCompose();
     }
 }
