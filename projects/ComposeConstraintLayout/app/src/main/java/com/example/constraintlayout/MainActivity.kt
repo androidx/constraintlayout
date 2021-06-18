ab9a7d08068a11329c58044bb4821f2c343fdd36
Rodrigo Dominguez <rodrigomartind@gmail.com>
2021-06-18 00:16:10 -0300

    Examples of MotionLayout in Compose (#255)
    * Examples of MotionLayout in Compose
    
    * Adjust UI Example15

--- projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -10,8 +10,8 @@ import androidx.constraintlayout.compose.*
 
 class MainActivity : AppCompatActivity() {
     private var mFrameLayout: FrameLayout? = null
-    private var composeNum = 15
-    private var MAX = 15
+    private var composeNum = 18
+    private var MAX = 19
 
     private fun show(com: ComposeView) {
         com.setContent() {
@@ -32,6 +32,9 @@ class MainActivity : AppCompatActivity() {
                 13 -> ScreenExample13()
                 14 -> ScreenExample14()
                 15 -> ScreenExample15()
+                16 -> ScreenExample16()
+                17 -> ScreenExample17()
+                18 -> ScreenExample18()
             }
         }
     }
