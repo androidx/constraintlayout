5d44781eb3cfcf4322ef32eb7ebe8579d1683b2b
Nicolas Roard <nicolasroard@google.com>
2021-06-22 21:06:39 -0700

    Update CLParserTest.java

--- constraintlayout/core/src/test/java/androidx/constraintlayout/core/parser/CLParserTest.java
+++ constraintlayout/core/src/test/java/androidx/constraintlayout/core/parser/CLParserTest.java
@@ -137,6 +137,7 @@ public class CLParserTest {
             CLObject parsedContent = CLParser.parse(test);
             assertEquals("John", parsedContent.getString("firstName"));
             assertEquals("{ firstName: 'John', lastName: 'Smith', isAlive: true, age: 27, address: { streetAddress: '21 2nd Street', city: 'New York', state: 'NY', postalCode: '10021-3100' }, phoneNumbers: [{ type: 'home', number: '212 555-1234' }, { type: 'office', number: '646 555-4567' }], children: [], spouse: null }", parsedContent.toJSON());
+            assertEquals(2, parsedContent.getArray("phoneNumbers").size());
         } catch (CLParsingException e) {
             System.err.println("Exception " + e.reason());
             e.printStackTrace();
