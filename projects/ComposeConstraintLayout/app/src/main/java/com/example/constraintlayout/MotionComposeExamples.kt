c8447ced7b5b322f1ce956aa99c8a82c66017f79
Nicolas Roard <nicolasroard@google.com>
2021-06-30 09:52:38 -0700

    wip

--- projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MotionComposeExamples.kt
+++ projects/ComposeConstraintLayout/app/src/main/java/com/example/constraintlayout/MotionComposeExamples.kt
@@ -5,12 +5,12 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.PlayArrow
@@ -19,12 +19,16 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.*
+import java.util.*
 
 @Preview(group = "motion1")
 @Composable
@@ -796,6 +800,141 @@ public fun MotionExample5() {
                 modifier = Modifier.layoutId("swipeUp").width(40.dp).height(40.dp)
             )
 
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview(group = "motion6")
+@Composable
+public fun MotionExample6() {
+    var animateToEnd by remember { mutableStateOf(false) }
+    val progress by animateFloatAsState(
+        targetValue = if (animateToEnd) 1f else 0f,
+        animationSpec = tween(2000)
+    )
+    var componentHeight by remember { mutableStateOf(600f) }
+    var height = 64.dp
+    val heightPx = with(LocalDensity.current) { height.toPx() }
+    val swipeableState = rememberSwipeableState("Bottom")
+    val anchors = mapOf(0f to "Bottom", componentHeight to "Top")
+
+    println("component height is $componentHeight")
+    println("progress fraction is " + swipeableState.progress.fraction)
+
+    Column {
+        Button(onClick = { animateToEnd = !animateToEnd }) {
+            Text(text = "Run")
+        }
+        MotionLayout(motionScene = MotionScene("""{
+                ConstraintSets: {
+                  start: {
+                    Variables: {
+                      texts: { tag: 'text' },
+                      margin: { from: 0, step: 50 }
+                    },
+                    Generate: {
+                      texts: {
+                        top: ['parent', 'top', 'margin'],
+                        start: ['parent', 'end', 16 ]
+                      }
+                    },
+                    box: {
+                      width: 'spread',
+                      height: 64,
+                      centerHorizontally: 'parent',
+                      bottom: ['parent','bottom']
+                    },
+                    content: {
+                      width: 'spread',
+                      height: '400',
+                      centerHorizontally: 'parent',
+                      top: ['box','bottom', 32]
+                    },
+                    name: {
+                      centerVertically: 'box',
+                      start: ['parent', 'start', 16]
+                    }
+                  },
+                  end: {
+                    Variables: {
+                      texts: { tag: 'text' },
+                      margin: { from: 0, step: 50 }
+                    },
+                    Generate: {
+                      texts: {
+                        start: ['parent','start', 32],
+                        top: ['content', 'top', 'margin']
+                      }
+                    },
+                    box: {
+                      width: 'spread',
+                      height: 200,
+                      centerHorizontally: 'parent',
+                      top: ['parent','top']
+                    },
+                    content: {
+                      width: 'spread',
+                      height: 'spread',
+                      centerHorizontally: 'parent',
+                      top: ['box','bottom'],
+                      bottom: ['parent', 'bottom']
+                    },
+                    name: {
+                      rotationZ: 90,
+                      scaleX: 2,
+                      scaleY: 2,
+                      end: ['parent', 'end', 16],
+                      top: ['parent', 'top', 90]
+                    }
+                  },
+                },
+                Transitions: {
+                  default: {
+                    from: 'start',
+                    to: 'end',
+                    pathMotionArc: 'startHorizontal',
+                    KeyFrames: {
+                      KeyAttributes: [
+                        {
+                          target: ['box','content'],
+                          frames: [50],
+                          rotationZ: [25],
+                          rotationY: [25], 
+                        }
+                      ]
+                    }
+                  }
+                }
+            }"""),
+            progress = swipeableState.progress.fraction,
+            debug = EnumSet.of(MotionLayoutDebugFlags.SHOW_ALL),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .swipeable(
+                    state = swipeableState,
+                    anchors = anchors,
+                    resistance = null,
+                    thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                    orientation = Orientation.Vertical
+                )
+                .onSizeChanged { size ->
+                    componentHeight = size.height.toFloat()
+                    println("SIZE CHANGE component height is $componentHeight")
+                }
+
+        ) {
+            Box(modifier = Modifier
+                .layoutId("content")
+                .background(Color.LightGray))
+            Box(modifier = Modifier
+                .layoutId("box")
+                .background(Color.Cyan))
+            Text(modifier = Modifier.layoutId("name"), text = "MotionLayout")
+            for (i in 0 until 6) {
+                Text(modifier = Modifier.layoutId("text$i", "text"), text = "Test $i")
+            }
         }
     }
 }
