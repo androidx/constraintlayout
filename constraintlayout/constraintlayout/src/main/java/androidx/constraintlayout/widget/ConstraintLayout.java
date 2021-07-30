a9af02fb2bcdfce780ed02a2a8f447ab56fc2357
John Hoford <hoford@google.com>
2021-07-30 11:21:47 -0700

    bump to 2.1.0

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
@@ -487,7 +487,7 @@ public class ConstraintLayout extends ViewGroup {
     /**
      * @hide
      */
-    public static final String VERSION = "ConstraintLayout-2.1.0-rc01";
+    public static final String VERSION = "ConstraintLayout-2.1.0";
     private static final String TAG = "ConstraintLayout";
 
     private static final boolean USE_CONSTRAINTS_HELPER = true;
