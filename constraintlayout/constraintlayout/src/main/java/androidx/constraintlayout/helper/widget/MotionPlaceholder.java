6c06c262eb9256cd8bdf3203244fd8b8fe490719
jafu888 <hoford@google.com>
2021-09-22 23:03:58 -0700

    fix xml_typo

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/helper/widget/MotionPlaceholder.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/helper/widget/MotionPlaceholder.java
@@ -19,7 +19,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.SparseArray;
-import android.view.ViewParent;
 
 import androidx.constraintlayout.core.widgets.ConstraintWidget;
 import androidx.constraintlayout.core.widgets.ConstraintWidgetContainer;
