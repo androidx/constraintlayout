5157a3e90f5803ad19d0df49e5748d1a1aa042b7
Jian-Syuan (Shane) Wong <shanewong@google.com>
2022-05-19 21:40:05 +0000

    version bump 2.1.4

--- constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
+++ constraintlayout/constraintlayout/src/main/java/androidx/constraintlayout/widget/ConstraintLayout.java
@@ -487,7 +487,7 @@ public class ConstraintLayout extends ViewGroup {
     /**
      * @suppress
      */
-    public static final String VERSION = "ConstraintLayout-2.1.3";
+    public static final String VERSION = "ConstraintLayout-2.1.4";
     private static final String TAG = "ConstraintLayout";
 
     private static final boolean USE_CONSTRAINTS_HELPER = true;
