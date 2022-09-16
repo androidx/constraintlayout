6a01570b9277a58b98d6ae31fb074d7771d4371f
Nicolas Roard <nicolas@roard.com>
2022-09-15 17:02:43 -0700

    replace Material swipe references (#725)
    * replace Material swipe references
    
    incorporate swipeable for now -- we might want to write something different.
    
    * rename packages / classes / functions + mark things as internal
    
    * Update MotionCarousel.kt

--- constraintlayout/compose/src/main/java/androidx/constraintlayout/compose/MotionCarousel.kt
+++ constraintlayout/compose/src/main/java/androidx/constraintlayout/compose/MotionCarousel.kt
@@ -22,10 +22,6 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -41,6 +37,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.MotionLayoutScope.MotionProperties
+
+// TODO: replace this implementation?
+import androidx.constraintlayout.compose.carousel.FractionalThreshold
+import androidx.constraintlayout.compose.carousel.rememberCarouselSwipeableState
+import androidx.constraintlayout.compose.carousel.carouselSwipeable
 
 /**
  * Implements an horizontal Carousel of n elements, driven by drag gestures and customizable
@@ -148,7 +149,7 @@ import androidx.constraintlayout.compose.MotionLayoutScope.MotionProperties
  * @param showSlots a debug flag to display the slots in the scene regardless if they are populated
  * @param content the MotionCarouselScope we use to map the elements to the slots
  */
-@OptIn(ExperimentalMotionApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMotionApi::class)
 @Composable
 fun MotionCarousel(
     motionScene: MotionScene,
@@ -168,7 +169,7 @@ fun MotionCarousel(
     val provider = rememberStateOfItemsProvider(content)
 
     var componentWidth by remember { mutableStateOf(1000f) }
-    val swipeableState = rememberSwipeableState(swipeStateStart)
+    val swipeableState = rememberCarouselSwipeableState(swipeStateStart)
     var mprogress = (swipeableState.offset.value / componentWidth)
 
     var state by remember {
@@ -241,7 +242,7 @@ fun MotionCarousel(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .swipeable(
+            .carouselSwipeable(
                 state = swipeableState,
                 anchors = anchors,
                 reverseDirection = true,
