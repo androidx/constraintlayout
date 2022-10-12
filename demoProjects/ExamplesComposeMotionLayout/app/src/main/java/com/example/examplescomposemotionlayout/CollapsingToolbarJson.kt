3154bc45c300b2cfb264555fd6a23b14b9818cdd
John Hoford <hoford@google.com>
2022-10-12 11:14:21 -0700

    add ExamplesComposeMotionLayout

---
+++ demoProjects/ExamplesComposeMotionLayout/app/src/main/java/com/example/examplescomposemotionlayout/CollapsingToolbarJson.kt
@@ -0,0 +1,138 @@
+package com.example.examplescomposemotionlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+
+
+
+
+
+
+@OptIn(ExperimentalMotionApi::class)
+@Preview(group = "scroll", device = "spec:shape=Normal,width=480,height=800,unit=dp,dpi=440")
+@Composable
+fun ToolBarExample() {
+    val scroll = rememberScrollState(0)
+
+    var scene = """
+      {
+        ConstraintSets: {
+          start: {
+            title: {
+              bottom: ['image', 'bottom', 16],                
+              start: [ 'image','start', 16],
+              },
+            image: {
+              width: 'parent',
+              height: 250,
+              top: ['parent', 'top', 0],
+              custom: {
+                cover: '#000000FF'
+              }
+            },
+            icon: {
+              top: ['image', 'top', 16],
+              start: [ 'image','start', 16],
+              alpha: 0,
+            },
+          },
+          end: {
+            title: {
+              centerVertically: 'image',
+              start: ['icon', 'end', 0],
+              scaleX: 0.7,
+              scaleY: 0.7,
+            },
+            image: {
+              width: 'parent',
+              height: 50,
+              top: ['parent', 'top', 0],
+              custom: {
+                cover: '#FF0000FF'
+              }
+            },
+            icon: {
+              top: ['image', 'top', 16],
+              start: [ 'image','start', 16],
+            },
+          },
+        },
+        Transitions: {
+          default: {
+            from: 'start',
+            to: 'end',
+            pathMotionArc: 'startHorizontal',
+          },
+        },
+      }
+      """
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.verticalScroll(scroll)
+    ) {
+        Spacer(Modifier.height(250.dp))
+        repeat(5) {
+            Text(
+                text = LoremIpsum(222).values.first(),
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(16.dp)
+            )
+        }
+    }
+
+    val progress = java.lang.Float.min(scroll.value / (3f * (250 - 50)), 1f);
+
+    MotionLayout(
+        modifier = Modifier.fillMaxSize(),
+        motionScene = MotionScene(content = scene),
+        progress = progress
+    ) {
+        Image(
+            modifier = Modifier.layoutId("image"),
+            painter = painterResource(R.drawable.bridge),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Box(modifier = Modifier
+            .layoutId("image")
+            .background(motionProperties("image").value.color("cover"))) {
+        }
+        Image(
+            modifier = Modifier.layoutId("icon"),
+            painter = painterResource(R.drawable.menu),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.layoutId("title"),
+            text = "San Francisco",
+            fontSize = 30.sp,
+            color = Color.White
+        )
+    }
+}
---
+++ demoProjects/ExamplesComposeMotionLayout/app/src/main/java/com/example/examplescomposemotionlayout/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.examplescomposemotionlayout
+
+import androidx.compose.ui.tooling.preview.Preview
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+import com.example.examplescomposemotionlayout.ui.theme.ExamplesComposeMotionLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    private val composeKey = "USE_COMPOSE"
+
+    private var cmap =   listOf(
+        get("CollapsingToolbarJson") {ToolBarExampleDsl()},
+        get("CollapsingToolbarDSL") {ToolBarExample()},
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val extra = intent.extras
+        var cfunc: ComposeFunc? = null
+        if (extra != null) {
+            val composeName = extra.getString(composeKey)
+            for (composeFunc in cmap) {
+                if (composeFunc.toString() == composeName) {
+                    cfunc = composeFunc
+                    break
+                }
+            }
+        }
+        setContent {
+            ExamplesComposeMotionLayoutTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    if (cfunc != null) {
+                        Log.v("MAIN", " running $cfunc")
+                        cfunc.Run()
+                    } else {
+                        ComposableMenu(map = cmap) { act -> launch(act) }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun launch(to_run: ComposeFunc) {
+        Log.v("MAIN", " launch $to_run")
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(composeKey, to_run.toString())
+        startActivity(intent)
+
+    }
+}
+
+@Composable
+fun ComposableMenu(map: List<ComposeFunc>, act: (act:ComposeFunc) -> Unit) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)) {
+        for (cFunc in map) {
+            Button(onClick = { act(cFunc)}) {
+                Text(cFunc.toString())
+            }
+        }
+    }
+}
+
+fun get(name: String, cRun:@Composable () ->  Unit):ComposeFunc {
+    return object: ComposeFunc {
+        @Composable
+        override fun Run() {
+            cRun()
+        }
+
+        override fun toString(): String {
+            return name
+        }
+    }
+}
+interface  ComposeFunc {
+    @Composable
+    fun Run()
+    override fun toString():String
+}
