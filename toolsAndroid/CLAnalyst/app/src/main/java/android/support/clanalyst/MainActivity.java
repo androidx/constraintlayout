ba4421a070a5165b3ab78bb754f9a0f8f44df334
John Hoford <hoford@google.com>
2022-12-16 12:32:08 -0800

    fixes and add the toolsAndroid

---
+++ toolsAndroid/CLAnalyst/app/src/main/java/android/support/clanalyst/MainActivity.java
@@ -0,0 +1,43 @@
+package android.support.clanalyst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout cl = findConstraintLayout(this);
+        if (cl != null) {
+            cl.postDelayed( ()->{DumpCL.log("MAIN",cl);},2000);
+        }
+    }
+
+    static ConstraintLayout findConstraintLayout(AppCompatActivity activity) {
+        ViewGroup group = ((ViewGroup) activity.findViewById(android.R.id.content).getRootView());
+        ArrayList<ViewGroup> groups = new ArrayList<>();
+        groups.add(group);
+        while (!groups.isEmpty()) {
+            ViewGroup vg = groups.remove(0);
+            int n = vg.getChildCount();
+            for (int i = 0; i < n; i++) {
+                View view = vg.getChildAt(i);
+                if (view instanceof ConstraintLayout) {
+                    return (ConstraintLayout) view;
+                }
+                if (view instanceof ViewGroup) {
+                    groups.add((ViewGroup) view);
+                }
+            }
+        }
+        return null;
+    }
+}
